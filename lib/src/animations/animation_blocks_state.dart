902f9b55097ecb9d76fd793325a15c3cc09616e6
Steve Gehrman <sgehrman@gmail.com>
2023-11-20 05:20:00 -0800

    sd

--- lib/src/animations/animation_blocks_state.dart
+++ lib/src/animations/animation_blocks_state.dart
@@ -11,7 +11,7 @@ class AnimationBlocksState {
   final double timeStart;
   final double timeEnd;
 
-  late final Animation<double> _animation;
+  late final BlockAnimations _animation;
 
   // expensive to alloc, create once
   final _sequence = TweenSequence<double>(
@@ -34,7 +34,7 @@ class AnimationBlocksState {
   // =================================================
 
   void initialize(AnimationController controller) {
-    _animation = _sequence.animate(
+    final blocks = Tween<double>(begin: 0, end: 1).animate(
       CurvedAnimation(
         parent: controller,
         curve: Interval(
@@ -43,6 +43,18 @@ class AnimationBlocksState {
         ),
       ),
     );
+
+    final opacity = _sequence.animate(
+      CurvedAnimation(
+        parent: controller,
+        curve: Interval(
+          timeStart,
+          timeEnd,
+        ),
+      ),
+    );
+
+    _animation = BlockAnimations(opacity: opacity, blocks: blocks);
   }
 
   Rect rectForIndex({
@@ -59,12 +71,21 @@ class AnimationBlocksState {
   }
 
   void paint(Canvas canvas, Size size) {
-    final rect = Offset.zero & size;
+    // final rect = Offset.zero & size;
 
     final blockPaint = Paint()
       ..color = Colors.cyan.withOpacity(
-        _animation.value,
+        _animation.opacity.value,
       );
+
+    // final rev = ReverseAnimation(_animation);
+
+    // final gridPaint = Paint()
+    //   ..color = Colors.cyan.withOpacity(
+    //     rev.value,
+    //   );
+
+    // canvas.drawRect(rect, gridPaint);
 
     final width = size.height / 6; // shortest side is height
 
@@ -73,7 +94,9 @@ class AnimationBlocksState {
 
     final numBlocks = cols * rows;
 
-    for (int i = 0; i < numBlocks; i++) {
+    final n = (_animation.blocks.value * numBlocks).ceil();
+
+    for (int i = 0; i < n; i++) {
       canvas.drawRect(
         rectForIndex(
           index: i,
@@ -83,7 +106,17 @@ class AnimationBlocksState {
         blockPaint,
       );
     }
-
-    canvas.drawRect(rect, blockPaint);
   }
+}
+
+// =====================================================
+
+class BlockAnimations {
+  BlockAnimations({
+    required this.opacity,
+    required this.blocks,
+  });
+
+  Animation<double> opacity;
+  Animation<double> blocks;
 }
