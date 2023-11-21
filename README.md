dd593023ed077a4a9fc06d633472790d599b21c9
Steve Gehrman <sgehrman@gmail.com>
2023-11-21 15:38:00 -0800

    sd

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Flutter animation experiments
 
 1. typewriter sounds
 2. how sync sound with animations?
+3. binocular view with focus?
--- lib/src/main_widget/deckr_animation.dart
+++ lib/src/main_widget/deckr_animation.dart
@@ -1,3 +1,4 @@
+import 'package:dfc_flutter/dfc_flutter_web.dart';
 import 'package:double07/src/animation_state.dart';
 import 'package:double07/src/audio_player/background_audio_player.dart';
 import 'package:double07/src/main_widget/deckr_animation_painter.dart';
@@ -81,12 +82,91 @@ class _DeckrAnimationState extends State<DeckrAnimation>
             }
 
             return const ColoredBox(
-              color: Colors.black87,
+              color: Colors.black,
             );
           },
         ),
         BackgroundAudioPlayer(autoplay: widget.autoplay),
+        Positioned(
+          bottom: 8,
+          left: 0,
+          right: 0,
+          child: _AnimationSlider(_controller),
+        ),
       ],
+    );
+  }
+}
+
+// ==========================================================
+
+class _AnimationSlider extends StatefulWidget {
+  const _AnimationSlider(this.controller);
+
+  final AnimationController controller;
+
+  @override
+  State<_AnimationSlider> createState() => _AnimationSliderState();
+}
+
+class _AnimationSliderState extends State<_AnimationSlider> {
+  @override
+  void initState() {
+    super.initState();
+
+    widget.controller.addListener(_listener);
+  }
+
+  @override
+  void dispose() {
+    widget.controller.removeListener(_listener);
+
+    super.dispose();
+  }
+
+  void _listener() {
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: Alignment.centerRight,
+      child: ConstrainedBox(
+        constraints: const BoxConstraints(maxWidth: 400),
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            DFIconButton(
+              color: Colors.cyan,
+              onPressed: () {
+                if (!widget.controller.isAnimating) {
+                  widget.controller.forward();
+                } else {
+                  widget.controller.stop();
+                }
+
+                setState(() {});
+              },
+              icon: Icon(
+                widget.controller.isAnimating ? Icons.stop : Icons.play_arrow,
+              ),
+            ),
+            Slider.adaptive(
+              value: widget.controller.value,
+              onChanged: (value) {
+                final wasRunning = widget.controller.isAnimating;
+
+                widget.controller.value = value;
+
+                if (wasRunning) {
+                  widget.controller.forward();
+                }
+              },
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
