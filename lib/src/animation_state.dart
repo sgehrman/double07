87ef3e103a0f3c8005dffb18c8f3160b34a7607a
Steve Gehrman <sgehrman@gmail.com>
2023-11-21 19:59:13 -0800

    sd

--- lib/src/animation_state.dart
+++ lib/src/animation_state.dart
@@ -1,14 +1,5 @@
 import 'package:double07/src/animations/animation_ball_state.dart';
-import 'package:double07/src/animations/backgrounds/anima_background.dart';
-import 'package:double07/src/animations/backgrounds/anima_background_state.dart';
-import 'package:double07/src/animations/shapes/anima_blocks.dart';
-import 'package:double07/src/animations/shapes/anima_blocks_state.dart';
-import 'package:double07/src/animations/shapes/anima_blocks_utils.dart';
-import 'package:double07/src/animations/text/anima_text.dart';
-import 'package:double07/src/animations/text/anima_text_state.dart';
 import 'package:double07/src/elements/anima_elements.dart';
-import 'package:double07/src/timeline.dart';
-import 'package:double07/src/utils.dart';
 import 'package:flutter/material.dart';
 
 abstract class RunableAnimation {
@@ -24,96 +15,15 @@ class AnimationState {
   bool isInitialized = false;
 
   final List<RunableAnimation> _runables = [
-    // background
-    // AnimaBackground(
-    //   AnimaBackgroundState(
-    //     imageAsset: '$kAssets/images/henderson.png',
-    //     gradientAlignment: const Alignment(-0.25, -0.3),
-    //     timeStart: 0,
-    //     timeEnd: 1,
-    //     mode: AnimaBackgroundMode.zoomIn,
-    //   ),
-    // ),
-
-    AnimaBackground(
-      AnimaBackgroundState(
-        imageAsset: '$kAssets/images/henderson.png',
-        gradientAlignment: const Alignment(-0.25, -0.3),
-        timeStart: Timeline.hendersonStart,
-        timeEnd: Timeline.hendersonEnd,
-      ),
-    ),
-
-    AnimaBackground(
-      AnimaBackgroundState(
-        imageAsset: '$kAssets/images/largo.jpg',
-        gradientAlignment: const Alignment(0.7, -0.1),
-        timeStart: Timeline.largoStart,
-        timeEnd: Timeline.largoEnd,
-      ),
-    ),
-
-    AnimaBackground(
-      AnimaBackgroundState(
-        imageAsset: '$kAssets/images/domino.jpg',
-        gradientAlignment: const Alignment(-0.1, -0.2),
-        timeStart: Timeline.dominoStart,
-        timeEnd: Timeline.dominoEnd,
-      ),
-    ),
-
-    // Deckr animation
-    AnimaText(
-      AnimaTextState(
-        line: AnimaTextLine(
-          text: 'Deckr'.toUpperCase(),
-          fontSize: 64,
-          bold: true,
-          color: Colors.white,
-        ),
-        alignments: [
-          const Alignment(-0.8, -2),
-          const Alignment(-0.8, -0.8),
-        ],
-        timeStart: Timeline.textStart,
-        timeEnd: Timeline.textEnd,
-      ),
-    ),
-
+    ...AnimaElements.backgrounds(),
+    AnimaElements.deckrLogo(),
     AnimaElements.hendersonQuote(),
     AnimaElements.largoQuote(),
     AnimaElements.dominoQuote(),
     AnimaElements.randomQuote(),
     AnimaElements.introTitles(),
-
-    // ball animation
+    ...AnimaElements.blocks(),
     AnimationBallState(),
-
-    AnimaBlocks(
-      AnimaBlocksState(
-        timeStart: Timeline.blocksStart,
-        timeEnd: Timeline.blocksEnd,
-        blocksSequence: AnimaBlocksUtils.blocksSequenceFull,
-        opacitySequence: AnimaBlocksUtils.opacitySequence,
-        flipSequence: AnimaBlocksUtils.flipSequence,
-        circles: true,
-        colorSequence: AnimaBlocksUtils.colorSequence(Colors.cyan, Colors.pink),
-        margin: 0,
-      ),
-    ),
-
-    AnimaBlocks(
-      AnimaBlocksState(
-        timeStart: Timeline.blocks2Start,
-        timeEnd: Timeline.blocks2End,
-        reverse: true,
-        downward: true,
-        numColumns: 3,
-        margin: 22,
-        blocksSequence: AnimaBlocksUtils.blocksSequence,
-        flipSequence: AnimaBlocksUtils.flipSequence,
-      ),
-    ),
   ];
 
   // =================================================
--- lib/src/elements/anima_elements.dart
+++ lib/src/elements/anima_elements.dart
@@ -1,7 +1,14 @@
 import 'package:double07/src/animation_state.dart';
+import 'package:double07/src/animations/backgrounds/anima_background.dart';
+import 'package:double07/src/animations/backgrounds/anima_background_state.dart';
+import 'package:double07/src/animations/shapes/anima_blocks.dart';
+import 'package:double07/src/animations/shapes/anima_blocks_state.dart';
+import 'package:double07/src/animations/shapes/anima_blocks_utils.dart';
 import 'package:double07/src/animations/text/anima_paragraph.dart';
+import 'package:double07/src/animations/text/anima_text.dart';
 import 'package:double07/src/animations/text/anima_text_state.dart';
 import 'package:double07/src/timeline.dart';
+import 'package:double07/src/utils.dart';
 import 'package:flutter/material.dart';
 
 class AnimaElements {
@@ -149,6 +156,94 @@ class AnimaElements {
           color: Colors.white,
         ),
       ],
+    );
+  }
+
+  static List<RunableAnimation> blocks() {
+    return [
+      AnimaBlocks(
+        AnimaBlocksState(
+          timeStart: Timeline.blocksStart,
+          timeEnd: Timeline.blocksEnd,
+          blocksSequence: AnimaBlocksUtils.blocksSequenceFull,
+          opacitySequence: AnimaBlocksUtils.opacitySequence,
+          flipSequence: AnimaBlocksUtils.flipSequence,
+          circles: true,
+          colorSequence:
+              AnimaBlocksUtils.colorSequence(Colors.cyan, Colors.pink),
+          margin: 0,
+        ),
+      ),
+      AnimaBlocks(
+        AnimaBlocksState(
+          timeStart: Timeline.blocks2Start,
+          timeEnd: Timeline.blocks2End,
+          reverse: true,
+          downward: true,
+          numColumns: 3,
+          margin: 22,
+          blocksSequence: AnimaBlocksUtils.blocksSequence,
+          flipSequence: AnimaBlocksUtils.flipSequence,
+        ),
+      ),
+    ];
+  }
+
+  static List<RunableAnimation> backgrounds() {
+    return [
+      // AnimaBackground(
+      //   AnimaBackgroundState(
+      //     imageAsset: '$kAssets/images/henderson.png',
+      //     gradientAlignment: const Alignment(-0.25, -0.3),
+      //     timeStart: 0,
+      //     timeEnd: 1,
+      //     mode: AnimaBackgroundMode.zoomIn,
+      //   ),
+      // ),
+
+      AnimaBackground(
+        AnimaBackgroundState(
+          imageAsset: '$kAssets/images/henderson.png',
+          gradientAlignment: const Alignment(-0.25, -0.3),
+          timeStart: Timeline.hendersonStart,
+          timeEnd: Timeline.hendersonEnd,
+        ),
+      ),
+      AnimaBackground(
+        AnimaBackgroundState(
+          imageAsset: '$kAssets/images/largo.jpg',
+          gradientAlignment: const Alignment(0.7, -0.1),
+          timeStart: Timeline.largoStart,
+          timeEnd: Timeline.largoEnd,
+        ),
+      ),
+      AnimaBackground(
+        AnimaBackgroundState(
+          imageAsset: '$kAssets/images/domino.jpg',
+          gradientAlignment: const Alignment(-0.1, -0.2),
+          timeStart: Timeline.dominoStart,
+          timeEnd: Timeline.dominoEnd,
+        ),
+      ),
+    ];
+  }
+
+  static RunableAnimation deckrLogo() {
+    return AnimaText(
+      AnimaTextState(
+        line: AnimaTextLine(
+          text: 'Deckr'.toUpperCase(),
+          fontSize: 64,
+          bold: true,
+          color: Colors.white,
+        ),
+        alignments: [
+          const Alignment(-0.8, -2),
+          const Alignment(-0.8, -0.8),
+        ],
+        timeStart: Timeline.textStart,
+        timeEnd: Timeline.textEnd,
+      ),
     );
   }
 }
